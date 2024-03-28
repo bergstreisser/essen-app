d72f214e4e5a4e0d9170a5eb1064d0551c49acaf
Bergi <info@bergstreisser.de>
2024-03-28 08:30:01 +0100

    Essen buchen

--- src/components/Essenkarte/index.jsx
+++ src/components/Essenkarte/index.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import styles from './Essenkarte.module.scss';
 import Calendar from "react-calendar";
+//import 'react-calendar/dist/Calendar.css';
 
 function Essenkarte({ id, bezeichnung, beschreibung, url, alt, setDate }) {
 
-    const onClickDate = date => {
-        //setDate(date);
-    }
+    const [cardVisible, setCardVisible] = React.useState(true);
+    const [calendarVisible, setCalendarVisible] = React.useState(false);
 
     const onClickBeschreibung = () => {
         alert(beschreibung);
     }
 
+    const changeVisibility = () => {
+        setCardVisible(!cardVisible);
+        setCalendarVisible(!calendarVisible);
+    }
+
     return (
         <div className={styles.essenkarte}>
-            <img onClick={onClickBeschreibung} className={styles.image} src={url} width={155} height={135} alt={alt} />
-            <h4><u>{bezeichnung}</u></h4>
+            <div>
+                {cardVisible && <img onClick={onClickBeschreibung} className={styles.image} src={url} width={155} height={135} alt={alt} />}
+                {cardVisible && <h4><u>{bezeichnung}</u></h4>}
+                {calendarVisible && <Calendar onChange={setDate} className={styles.calendar} />}
+            </div>
             <div className={styles.buchen}>
                 <p>Essen buchen:</p>
-                <img onClick={onClickDate} className={styles.add} src="/img/add.png" alt="add" />
-                {/* <Calendar onChange={setDate} /> */}
+                <img onClick={changeVisibility} className={styles.add} src="/img/add.png" alt="add" />
             </div>
         </div>
 
