5a91799e9dfc43df56cfa46e7f3f4d0bad80c29f
Bergi <info@bergstreisser.de>
2024-03-28 18:08:07 +0100

    Kelender

--- src/components/Essenkarte/index.jsx
+++ src/components/Essenkarte/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './Essenkarte.module.scss';
 import Calendar from "react-calendar";
+import moment from 'moment';
+import 'moment/locale/de'  
 //import 'react-calendar/dist/Calendar.css';
 
 function Essenkarte({ id, bezeichnung, beschreibung, url, alt, setDate }) {
@@ -22,7 +24,16 @@ function Essenkarte({ id, bezeichnung, beschreibung, url, alt, setDate }) {
             <div>
                 {cardVisible && <img onClick={onClickBeschreibung} className={styles.image} src={url} width={155} height={135} alt={alt} />}
                 {cardVisible && <h4><u>{bezeichnung}</u></h4>}
-                {calendarVisible && <Calendar onChange={setDate} className={styles.calendar} />}
+                {calendarVisible && <Calendar className={styles.calendar}
+                    onChange={setDate}
+                    prev2Label={""}
+                    next2Label={""}
+                    prevLabel={"«"}
+                    nextLabel={"»"}
+                    format={"YY/MM"}
+                    formatMonthYear ={(locale, date) => moment(date).local('de').format('MMM YYYY')}
+                    formatShortWeekday = {(locale, date) => ['S', 'M', 'D', 'M', 'D', 'F', 'S'][date.getDay()]}
+                />}
             </div>
             <div className={styles.buchen}>
                 <p>Essen buchen:</p>
